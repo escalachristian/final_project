98dfd05ad61690c4a81aaf9505f32a95ed8d7e73
Christian Escala <Christian@Christians-MacBook-Air.local>
2018-04-12 19:25:42 -0400

    getting rid of the nav as suggested by prof, adding to js file so users can search recipes by ingredient

--- js/data-json.js
+++ js/data-json.js
@@ -1,8 +1,18 @@
+function bindImageClick(id){
+	var image = document.getElementById(id);
+	image.addEventListener('click', function(){submitForm(id)}, false);
+}
+
+function submitForm(id){
+	document.getElementById("url").value = document.getElementById(id).getAttribute('src');
+	document.getElementById("recipe-page").submit();
+}
+
 // create XMLHttpRequest object
 var xhr = new XMLHttpRequest();
 
 // when  response has loaded 
-xhr.onload = function() {
+xhr.onload = function() {	
 	//if (xhr.status === 200) {
 		responseObject = JSON.parse(xhr.responseText);
 
@@ -10,10 +20,10 @@ xhr.onload = function() {
 		var newContent = '';
 		for (var i = 0; i < responseObject.recipes.length; i++) {
 			//create opening div tag
-			newContent += '<div class="recipe">';
+			newContent += '<div class="recipe" data="'+ responseObject.recipes[i].ingredient +'">';
 			//create img with alt location
-			newContent += '<a href="recipe.html"><img src="' + responseObject.recipes[i].photo + '"';
-			newContent += 'alt"=' + responseObject.recipes[i].name + '"/></a>';
+			newContent += '<img id="image-' + i + '" src="' + responseObject.recipes[i].photo + '"';
+			newContent += 'alt"=' + responseObject.recipes[i].name + '"/>';
 			//write out location <a href="default.asp">
 			//newContent += '<p><b>' + responseObject.events[i].location + '</b><br>';
 			//write out date
@@ -24,6 +34,10 @@ xhr.onload = function() {
 
 		//udate the page with our new content
 		document.getElementById('content').innerHTML = newContent;
+		
+		for (var i=0; i<responseObject.recipes.length; i++) {
+			bindImageClick("image-" + i);
+		}
 
 	//}
 };
@@ -32,4 +46,25 @@ xhr.onload = function() {
 xhr.open('GET', 'data/data.json', true);
 
 xhr.send(null);
+
+var searchButton = document.getElementById("ingredient");
+searchButton.addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    var tiles = document.getElementsByClassName("recipe");
+    if (key === 13) { // 13 is enter
+      var searchText = searchButton.value;
+      for (var i=0; i< tiles.length; i++) {
+      	if(tiles[i].getAttribute('data').toLowerCase()!= searchText.toLowerCase()) {
+      		tiles[i].style.display = "none";
+      	} else {
+      		tiles[i].style.display = "inline-block";
+      	}
+      }
+    } else if (key === 8 || key === 46) { // Backspace or Delete
+    	for (var i=0; i< tiles.length; i++) {
+        	tiles[i].style.display = "inline-block";	
+      }
+    }
+}, false);
+
 
