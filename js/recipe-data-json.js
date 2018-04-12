29a12d92dcfe851aefdebb0a8e606984017ba4a9
Christian Escala <Christian@Christians-MacBook-Air.local>
2018-04-12 19:44:54 -0400

    I moved around the files fromy my pc and now I'm troubleshooting to connect them again

--- js/recipe-data-json.js
+++ js/recipe-data-json.js
@@ -1,34 +1,8 @@
-// create XMLHttpRequest object
-var xhr = new XMLHttpRequest();
+function get(name){
+   if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
+      return decodeURIComponent(name[1]);
+}
 
-// when  response has loaded 
-xhr.onload = function() {
-	//if (xhr.status === 200) {
-		responseObject = JSON.parse(xhr.responseText);
 
-		// build up a string with a new content (can also use DOM manipulation)
-		var newContent = '';
-		for (var i = 0; i < responseObject.recipes.length; i++) {
-			//create opening div tag
-			newContent += '<div class="recipe">';
-			//create img with alt location
-			newContent += '<img src="' + responseObject.recipes[i].photo + '"';
-			newContent += 'alt"=' + responseObject.recipes[i].name + '"/>';
-			//write out location <a href="default.asp">
-			//newContent += '<p><b>' + responseObject.events[i].location + '</b><br>';
-			//write out date
-			//newContent += responseObject.events[i].date + '</p>';
-			//close the div tag
-			newContent += '</div>';
-		}
+document.getElementById("banner-image").setAttribute('src', get('url'));
 
-		//udate the page with our new content
-		document.getElementById('recipe-content').innerHTML = newContent;
-
-	//}
-};
-
-//prepare the request
-xhr.open('GET', 'data/data.json', true);
-
-xhr.send(null);
